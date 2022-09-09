64139d9f82627253804fcb16ccb209145b1fa1fe
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-09 16:14:30 -0300

    Testando o repositorio de Cidades

--- src/main/java/io/github/moreiranat/localizacao/LocalizacaoApplication.java
+++ src/main/java/io/github/moreiranat/localizacao/LocalizacaoApplication.java
@@ -1,15 +1,33 @@
 package io.github.moreiranat.localizacao;
 
+import io.github.moreiranat.localizacao.domain.entity.Cidade;
+import io.github.moreiranat.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Inicializado!");
+		salvarCidade();
+		listarCidades();
+	}
+
+	@Transactional
+	void salvarCidade() {
+		var cidade = new Cidade(1L, "São Paulo",12396372L);
+		cidadeRepository.save(cidade);
+	}
+
+	void listarCidades() {
+		cidadeRepository.findAll().forEach(System.out::println); //para cada cidade, vai ser executado esse metodo
 	}
 
 	public static void main(String[] args) {
--- src/main/java/io/github/moreiranat/localizacao/domain/entity/Cidade.java
+++ src/main/java/io/github/moreiranat/localizacao/domain/entity/Cidade.java
@@ -1,12 +1,16 @@
 package io.github.moreiranat.localizacao.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "tb_cidade")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cidade {
 
     @Id
